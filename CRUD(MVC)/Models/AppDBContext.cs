c9393de2539f1355c727dccdf21ae8acf61bdbf8
ilgyha228 <ilgyha228@gmail.com>
2022-05-07 18:30:37 +0300

    Добавьте файлы проекта.

---
+++ CRUD(MVC)/Models/AppDBContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace CRUD_MVC_.Models
+{
+    public class AppDBContext:DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options)
+            : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
---
+++ CRUD(MVC)/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace CRUD_MVC_.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public string Model { get; set; }
+        public string Display { get; set; }
+        public string Category { get; set; }
+
+    }
+}
